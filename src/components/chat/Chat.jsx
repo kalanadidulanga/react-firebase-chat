fefcb9d8bf2f4cc31afb38d78294ec1ca1797334
Kalana Coder <91042617+kalanadidulanga@users.noreply.github.com>
2024-07-20 01:31:46 +0530

    detail start

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,107 @@
+import { useState, useRef, useEffect } from 'react'
+import './chat.css'
+import EmojiPicker from 'emoji-picker-react'
+
+const Chat = () => {
+  const [open, setOpen] = useState(false)
+  const [text, setText] = useState("")
+  const emojiRef = useRef(null)
+
+  const handleEmoji = (e) => {
+    setText(prev => prev + e.emoji)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (emojiRef.current && !emojiRef.current.contains(event.target)) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
+  return (
+    <div className='chat'>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Kalana Didulanga</span>
+            <p>Lorem ipsum, dolor sit amet mporibus exercitationem aliq sciunt ex?</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, voluptatem?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, voluptatem?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+          <img src={"https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp"} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, voluptatem?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, voluptatem?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src={"https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg"} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, voluptatem?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, voluptatem?</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input type="text" placeholder='Type a message...' onChange={(e) => setText(e.target.value)} value={text} />
+        <div className="emoji" ref={emojiRef}>
+          <img src="./emoji.png" alt="" onClick={() => setOpen(prev => !prev)} />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} searchPlaceHolder='' />
+          </div>
+        </div>
+        <button className='sendButton'>Send</button>
+      </div>
+    </div>
+  )
+}
+
+export default Chat
