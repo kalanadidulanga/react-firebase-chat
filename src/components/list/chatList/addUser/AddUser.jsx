18bb94fff58d7bacc647d29f9582a08964f35802
Kalana Coder <91042617+kalanadidulanga@users.noreply.github.com>
2024-07-21 18:40:31 +0530

    asdasd

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,20 +1,51 @@
+import { collection, getDocs, query, where } from 'firebase/firestore'
 import './addUser.css'
+import { db } from '../../../../lib/firebase'
+import { useState } from 'react'
 
 const AddUser = () => {
+  const [user, setUser] = useState(null);
+
+  const handleAdd = async () => {
+
+  }
+
+  const handleUser = async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const username = formData.get('username')
+
+    try {
+      const userRef = collection(db, 'users')
+
+      const q = query(userRef, where('username', '==', username))
+      const querySnapshot = await getDocs(q)
+
+      if (!querySnapshot.empty) {
+        setUser(querySnapshot.docs[0].data())
+      }
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className='addUser'>
-        <form >
-            <input type="text" placeholder='Username...' name='username' />
-            <button>Search</button>
-        </form>
+      <form onSubmit={handleUser}>
+        <input type="text" placeholder='Username...' name='username' />
+        <button type='submit'>Search</button>
+      </form>
 
+      {user &&
         <div className='user'>
-            <div className="detail">
-                <img src="./avatar.png" alt="" />
-                <span>Kalana Didulanga</span>
-            </div>
-            <button>Add User</button>
+          <div className="detail">
+            <img src={user.avatar || "./avatar.png"} alt="" />
+            <span>{user.username}</span>
+          </div>
+          <button onClick={handleAdd}>Add User</button>
         </div>
+      }
     </div>
   )
 }
